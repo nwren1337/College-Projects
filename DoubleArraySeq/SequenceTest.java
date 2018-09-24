4eb1afd87f087b338220dda69f04d9a940a9246a
yeetnskeet <nwren92@gmail.com>
2018-09-24 03:38:54 -0400

    Created class to test ADT

---
+++ DoubleArraySeq/SequenceTest.java
@@ -0,0 +1,16 @@
+/**
+ * Class to test out functionality of the DoubleArraySeq Abstract Data Type.
+ * @author Nathan Wren
+ * @author Caleb Secor
+ * @version 0.1
+ * 9/24/2018
+ */
+ 
+
+public class SequenceTest
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello.");
+   }
+}
