20b119a274d34015d85fda518a2b12f2e9a3dd73
Nate Wren <nwren@datto.com>
2018-09-27 00:13:48 -0400

    Started implementing driver file

--- DoubleArraySeq/SequenceTest.java
+++ DoubleArraySeq/SequenceTest.java
@@ -6,68 +6,182 @@
  * 9/24/2018
  */
  
+import java.util.*;
 
 public class SequenceTest
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello.");
-/*      
-      DoubleArraySeq test = new DoubleArraySeq();
-      test.addFront(1);
-      System.out.println(test.getCurrent());
-      test.addFront(3);
-      System.out.println(test.getCurrent());
-      test.advance();
-      System.out.println(test.getCurrent());
-      test.advance();
-      test.addBefore(2);
-      System.out.println(test.getCurrent());
-      test.advance();
-      System.out.println(test.getCurrent());
-      test.addEnd(4);
-      System.out.println(test.getCurrent());
+      int menuOption = 0;
+      boolean cont = true;
+      char currSeq = 'A';
+      final int INIT_SIZE = 20;
+      Scanner keyboard = new Scanner(System.in);
       
-      DoubleArraySeq test2 = new DoubleArraySeq();
-      test2.addFront(1);
-      test2.addFront(3);
-      test2.advance();
-      test2.advance();
-      test2.addBefore(2);
-      test2.addEnd(4);
+      DoubleArraySeq sequenceA = new DoubleArraySeq();
+      DoubleArraySeq sequenceB = new DoubleArraySeq(INIT_SIZE);
       
-      System.out.println(test.equals(test2));*/
+      System.out.println("Hello! This program is intended to test the functionality of the DoubleArraySeq abstract data type");
+      System.out.println("Two sequences were initialized, a default one and a one of size 20.");
       
-      DoubleArraySeq test3 = new DoubleArraySeq();
-      
-      for(int i = 0; i < 10; i++)
-      {
-         test3.addEnd(i);
-      }
-      
-      System.out.println("Size of data : " + test3.getCapacity() + " manyItems : " + test3.size());
-      
-      double element = test3.getElement(4);
-      
-      System.out.println("4th element : " + element);
-      
-      
-      DoubleArraySeq test4 = new DoubleArraySeq();
-      
-      for(int i = 10; i < 20; i++)
-      {
-         test4.addEnd(i);
-      }
-      
-      DoubleArraySeq testConcat = DoubleArraySeq.concatenation(test3, test4);
-      
-      testConcat.start();
-      
-      for(int i = 0; i < testConcat.size(); i++)
-      {
-         System.out.println(testConcat.getCurrent());
-         testConcat.advance();
-      }
-      
+      do {
+         try {
+            menuOption = mainMenu(currSeq, keyboard);
+            
+            switch(menuOption)
+            {
+               case 1: printBoth(sequenceA, sequenceB);
+                       break;
+               case 2: printCapacity(sequenceA, sequenceB);
+                       break;
+               case 3: testEquality(sequenceA, sequenceB);
+                       break;
+               case 4: currSeq = swapActive(currSeq);
+                       break;
+               case 5: addToFront(currSeq, sequenceA, sequenceB, keyboard);
+                       break;
+               case 6: addNumBefore(currSeq, sequenceA, sequenceB, keyboard);
+                       break;
+               case 7: //addNumAfter(currSeq, sequenceA, sequenceB, keyboard);
+                       break;
+               case 8: //addNumToEnd(currSeq, sequenceA, sequenceB, keyboard);
+                       break;
+               case 9: //addBtoA(sequenceA, sequenceB);
+                       break;
+               case 10: //deleteAtIndex(currSeq, sequenceA, sequenceB, keyboard);
+                        break;
+               case 11: //deleteFirst(currSeq, sequenceA, sequenceB, keyboard);
+                        break;
+               case 12: //displayAtIndex(currSeq, sequenceA, sequenceB, keyboard);
+                        break;
+               case 13: //displayLast(currSeq, sequenceA, sequenceB);
+                        break;
+               case 14: //trimSequence(currSeq, sequenceA, sequenceB);
+                        break;
+               case 15: //cloneSequence(currSeq, sequenceA, sequenceB);
+                        break;
+               case 16: //mergeSequence(sequenceA, sequenceB);
+                        break;
+               case 17: cont = false;
+                        break;
+               default:
+                        System.out.println("Invalid Input!");
+                        break;
+            }
+            
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            keyboard.next();
+        }
+     } while(cont);  
+           
+    System.out.println("Have a nice day!");
    }
+   
+   public static void printBoth(DoubleArraySeq A, DoubleArraySeq B)
+   {
+      System.out.println("Sequence A : " + A + "\nSequence B : " + B);
+   }
+   
+   public static void printCapacity(DoubleArraySeq A, DoubleArraySeq B)
+   {
+      System.out.println("Sequence A capacity : " + A.getCapacity() + "\nSequence B : " + B.getCapacity());
+   }
+   
+   public static void testEquality(DoubleArraySeq A, DoubleArraySeq B)
+   {
+      if(A.equals(B))
+         System.out.println("Sequence A is equal to Sequence B");
+      else
+         System.out.println("The sequences are not equal!");
+   }
+   
+   public static char swapActive(char curr)
+   {
+      char ret;
+      ret = (curr == 'A') ? 'B' : 'A';
+      return ret;
+   }
+   
+   public static void addNumBefore(char currSeq, DoubleArraySeq A, DoubleArraySeq B, Scanner keyboard) throws InputMismatchException
+   {
+      double numToAdd;
+      int index;
+      if(currSeq == 'A')
+      {
+         System.out.println("There are " + A.size() + " elements in sequence A, which number do you want to insert before : ");
+         index = keyboard.nextInt();
+         
+         if(index <= A.size())
+         {
+            System.out.println("Inserting before the " + index + " element.");
+            A.setCurrent(index - 1);
+            
+            System.out.print("What number do you want to insert : ");
+            numToAdd = keyboard.nextDouble();
+            
+            A.addBefore(numToAdd);
+         } else {
+            System.out.println("Out of range of A!");
+         }
+      } else {
+         System.out.println("There are " + B.size() + " elements in sequence B, which number do you want to insert before : ");
+         index = keyboard.nextInt();
+         
+         if(index <= B.size())
+         {
+            System.out.println("Inserting before the " + index + " element.");
+            B.setCurrent(index - 1);
+            
+            System.out.print("What number do you want to insert : ");
+            numToAdd = keyboard.nextDouble();
+            
+            B.addBefore(numToAdd);
+         } else {
+            System.out.println("Out of range of B!");
+         }
+      }
+   }
+   
+   public static void addToFront(char currSeq, DoubleArraySeq A, DoubleArraySeq B, Scanner keyboard) throws InputMismatchException
+   {
+      double numToAdd;
+      System.out.print("Please enter the number you would like to add to the front of " + currSeq + " :");
+      numToAdd = keyboard.nextDouble();
+      
+      if(currSeq == 'A')
+         A.addFront(numToAdd);
+      else
+         B.addFront(numToAdd);
+   }
+      
+   
+   public static int mainMenu(char curr, Scanner keyboard) throws InputMismatchException
+   {
+      int input;
+      
+      System.out.println("The current sequence is " + curr);
+      System.out.println("1.	Print the sequences of A and B");
+      System.out.println("2.	Report the capacity of A and B");
+      System.out.println("3.	Report if A and B are equal");
+      System.out.println("4.	Change active sequence");
+      System.out.println("5.	Add a number to the front of a sequence");
+      System.out.println("6.	Add a number before a given number");
+      System.out.println("7.	Add a number after a given number");
+      System.out.println("8.	Add a number to the end of a sequence");
+      System.out.println("9.	Add sequence B to end of A");
+      System.out.println("10.	Delete a number at a certain index");
+      System.out.println("11.	Delete the first number from the sequence");
+      System.out.println("12.	Display a number at a certain index");
+      System.out.println("13.	Display the last element in the sequence");
+      System.out.println("14.	Trim extra memory from both A and B");
+      System.out.println("15.	Create a clone sequence and show");
+      System.out.println("16.	Create a new sequence using concatenate of B and A and show");
+      System.out.println("17.	Quit");
+      System.out.print("Please enter option : ");
+      input = keyboard.nextInt();
+      
+      return input;
+   }
+      
 }
