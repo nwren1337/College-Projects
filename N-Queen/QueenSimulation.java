9bc15f81108dc9733c537bd87d074a704d102720
yeetnskeet <nwren92@gmail.com>
2018-10-26 22:30:19 -0400

    Tested additional functions

--- N-Queen/QueenSimulation.java
+++ N-Queen/QueenSimulation.java
@@ -9,5 +9,43 @@ public class QueenSimulation
       System.out.println("Queen 1 : " + test1);
       System.out.println("Queen 2 : " + test2);
       System.out.println("Conflicts : " + test1.conflicts(test2));
+      
+      if(!test1.down())
+         System.out.println("Could not move Queen 1 down");
+      
+      System.out.println("Queen 1 : " + test1);
+      System.out.println("Queen 2 : " + test2);
+      
+      if(test2.right())
+      {
+         System.out.println("Moved Queen 2 to the right");
+      }
+      
+      System.out.println("Queen 1 : " + test1);
+      System.out.println("Queen 2 : " + test2);
+      
+      if(test1.up())
+      {
+         System.out.println("Moved Queen 1 up");
+      }
+      
+      System.out.println("Queen 1 : " + test1);
+      System.out.println("Queen 2 : " + test2);
+      
+      if(test2.left())
+      {
+         System.out.println("Moved Queen 2 left");
+      }
+      
+      System.out.println("Queen 1 : " + test1);
+      System.out.println("Queen 2 : " + test2);
+      
+      if(test1.conflicts(test2))
+      {
+         System.out.println("Queen 1 conflicts with Queen 2");
+      } else {
+         System.out.println("Queen 1 does not conflict with Queen 2");
+      }
+         
    }
 }
