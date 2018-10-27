182c4ad23a16a9853e841652fcefa47a50fcd6a9
yeetnskeet <nwren92@gmail.com>
2018-10-26 21:48:59 -0400

    Tested both constructors, toString, and conflicts

--- N-Queen/QueenSimulation.java
+++ N-Queen/QueenSimulation.java
@@ -2,6 +2,12 @@ public class QueenSimulation
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello world");
+      Queen test1 = new Queen(4);
+      
+      Queen test2 = new Queen(0, 2, 4);
+      
+      System.out.println("Queen 1 : " + test1);
+      System.out.println("Queen 2 : " + test2);
+      System.out.println("Conflicts : " + test1.conflicts(test2));
    }
 }
