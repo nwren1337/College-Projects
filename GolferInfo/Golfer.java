42b6da06aa68f43159bdf850d44e4673503ed770
yeetnskeet <nwren92@gmail.com>
2018-11-16 16:11:31 -0500

    implemented constructors in Golfer

--- GolferInfo/Golfer.java
+++ GolferInfo/Golfer.java
@@ -1,4 +1,36 @@
+/**
+ * Golfer class to model information on a golfer. Tracks last name,
+ * number of rounds, handicap, and average score of the golfer.
+ *@author Nathan Wren
+ *@version 0.1
+ *@since 11/16/2018
+ */
 public class Golfer
 {
-   /* to be implemented */
+   //Invariant of the Golfer class :
+   // 1. Last name is the object used to implement compareTo.
+   // 2. All numerical data members are updated together. Ie,
+   //    if a new round is added, the average score and handicap
+   //    are recalculated  
+   private String lastName;
+   private int numRounds;
+   private int handicap;
+   private double avgScore;
+   
+   public Golfer(String name)
+   {
+      lastName = name;
+      numRounds = 0;
+      handicap = 0;
+      avgScore = 0;
+   }
+   
+   public Golfer(String name, int rounds, int h, double sc)
+   {
+      lastName = name;
+      numRounds = rounds;
+      handicap = h;
+      avgScore = sc;
+   }
+   
 }
