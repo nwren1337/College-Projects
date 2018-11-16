f1dc916581bc2f07ddf9856419b1ccfcc3e99023
yeetnskeet <nwren92@gmail.com>
2018-11-16 16:46:47 -0500

    Added a bunch of tests for stuff

--- GolferInfo/GolferScoresTree.java
+++ GolferInfo/GolferScoresTree.java
@@ -3,6 +3,33 @@ public class GolferScoresTree
    /* driver file */
    public static void main(String[] args)
    {
-      System.out.println("Hello World");
+      Golfer test1 = new Golfer("Aaron");
+      Golfer test2 = new Golfer("Adam");
+      Golfer test3 = new Golfer("Bill");
+      Golfer test4 = new Golfer("Sam");
+      Golfer test5 = new Golfer("Sam");
+      
+      if(test1.compareTo(test2) < 0)
+      {
+         System.out.println(test1.getName() + " is less than " + test2.getName());
+      }
+      
+      if(test3.compareTo(test2) > 0)
+      {
+         System.out.println(test3.getName() + " is greater than " + test2.getName());
+      }
+      
+      if(test4.compareTo(test5) == 0)
+      {
+         System.out.println(test4.getName() + " is equal to " + test5.getName()); 
+      }
+      
+      //Golfer test6 = new Golfer("Tom", 3, -1, 44.0); //Throws exception as expected
+      //Golfer test6 = new Golfer("Tom", 3, 55, 44.0); //Throws exception as expected
+      Golfer test6 = new Golfer("Tom", 3, 5, 44.0); //Does not throw exception 
+      
+      System.out.println(test6.getName() + " played " + test6.getNumRounds() + " rounds with a handicap of " + test6.getHandicap() + " and average score of " + test6.getAverageScore());
+      
+      
    }
 }
