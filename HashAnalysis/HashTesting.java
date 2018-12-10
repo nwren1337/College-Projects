d0e177be824116d39f09725377300811a3c63d2c
yeetnskeet <nwren92@gmail.com>
2018-12-09 21:18:52 -0500

    Tested open address hash table with first 25 elements from input file

--- HashAnalysis/HashTesting.java
+++ HashAnalysis/HashTesting.java
@@ -1,7 +1,55 @@
+//Imports for I/O  
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class HashTesting
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello world");
+      Table test = new Table<Integer, String>(31);
+      String filename = "names.txt";
+      
+      //Variables to handle the file I/O
+      Scanner inputStream;
+      File f;
+      
+      //Temp variables to store value from the line
+      String name, temp;
+      int key;
+      int[] collisions = new int[25];
+      
+      //Temp variable for splitting the line from the file into individual components
+      String[] line;
+      
+      try 
+      {
+         //Initialize input stream
+         f = new File(filename);
+         inputStream = new Scanner(f);
+         
+         for(int i = 0; i < 25; i++)
+         {
+            temp = inputStream.nextLine();
+            line = temp.split(" ");
+            name = line[0];
+            key = Integer.parseInt(line[1]);
+            
+            collisions[i] = test.put(key, name);
+         }
+      } catch (Exception e) {
+         System.out.println("Error: " + e.getClass().getCanonicalName() + "\n" + e.getMessage());
+      }
+      
+      System.out.println("Collisions per attempted placement in table :");
+      System.out.println("Attempt\tLinear\tDouble\tChain");
+      
+      for(int i = 0; i < 25; i++)
+      {
+         System.out.println(i + "\t" + collisions[i]);
+      }
+     
    }
 }
